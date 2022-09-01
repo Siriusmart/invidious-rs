6a1c3061443d03178f77511474d945d1af58325b
Siriusmart <71584876+Siriusmart@users.noreply.github.com>
2022-09-01 12:38:23 +0100

    Fixed inconsistencies in casing

--- src/structs/hidden/searchitem.rs
+++ src/structs/hidden/searchitem.rs
@@ -1,5 +1,3 @@
-#![allow(non_snake_case)]
-
 use crate::structs::hidden::{AuthorThumbnail, VideoThumbnail};
 use serde::{Deserialize, Serialize};
 
@@ -7,27 +5,40 @@ use serde::{Deserialize, Serialize};
 pub struct SearchItemTransition {
     pub r#type: String,
     pub title: Option<String>,
-    pub videoId: Option<String>,
+    #[serde(rename(serialize = "videoId", deserialize = "videoId"))]
+    pub video_id: Option<String>,
     pub author: String,
-    pub authorId: String,
-    pub authorUrl: String,
-    pub videoThumbnails: Option<Vec<VideoThumbnail>>,
+    #[serde(rename(serialize = "authorId", deserialize = "authorId"))]
+    pub author_id: String,
+    #[serde(rename(serialize = "authorUrl", deserialize = "authorUrl"))]
+    pub author_url: String,
+    #[serde(rename(serialize = "videoThumbnails", deserialize = "videoThumbnails"))]
+    pub video_thumbnails: Option<Vec<VideoThumbnail>>,
     pub description: Option<String>,
-    pub descriptionHtml: Option<String>,
-    pub viewCount: Option<u64>,
+    #[serde(rename(serialize = "descriptionHtml", deserialize = "descriptionHtml"))]
+    pub description_html: Option<String>,
+    #[serde(rename(serialize = "viewCount", deserialize = "viewCount"))]
+    pub view_count: Option<u64>,
     pub published: Option<u64>,
-    pub publishedText: Option<String>,
-    pub lengthSeconds: Option<u64>,
-    pub liveNow: Option<bool>,
+    #[serde(rename(serialize = "publishedText", deserialize = "publishedText"))]
+    pub published_text: Option<String>,
+    #[serde(rename(serialize = "lengthSeconds", deserialize = "lengthSeconds"))]
+    pub length_seconds: Option<u64>,
+    #[serde(rename(serialize = "liveNow", deserialize = "liveNow"))]
+    pub live_now: Option<bool>,
     pub paid: Option<bool>,
     pub premium: Option<bool>,
 
-    pub playlistId: Option<String>,
-    pub videoCount: Option<u32>,
+    #[serde(rename(serialize = "playlistId", deserialize = "playlistId"))]
+    pub playlist_id: Option<String>,
+    #[serde(rename(serialize = "videoCount", deserialize = "videoCount"))]
+    pub video_count: Option<u32>,
     pub videos: Option<Vec<SearchPlaylistVideo>>,
 
-    pub authorThumbnails: Option<Vec<AuthorThumbnail>>,
-    pub subCount: Option<u32>,
+    #[serde(rename(serialize = "authorThumbnails", deserialize = "authorThumbnails"))]
+    pub author_thumbnails: Option<Vec<AuthorThumbnail>>,
+    #[serde(rename(serialize = "subCount", deserialize = "subCount"))]
+    pub sub_count: Option<u32>,
 }
 
 impl SearchItemTransition {
@@ -35,40 +46,40 @@ impl SearchItemTransition {
         match self.r#type.as_str() {
             "video" => SearchItem::Video {
                 title: self.title.unwrap_or(String::new()),
-                videoId: self.videoId.unwrap_or(String::new()),
-                lengthSeconds: self.lengthSeconds.unwrap_or(0),
-                videoThumbnails: self.videoThumbnails.unwrap_or(Vec::new()),
+                id: self.video_id.unwrap_or(String::new()),
+                length: self.length_seconds.unwrap_or(0),
+                thumbnails: self.video_thumbnails.unwrap_or(Vec::new()),
                 description: self.description.unwrap_or(String::new()),
-                descriptionHtml: self.descriptionHtml.unwrap_or(String::new()),
-                viewCount: self.viewCount.unwrap_or(0),
+                description_html: self.description_html.unwrap_or(String::new()),
+                views: self.view_count.unwrap_or(0),
                 published: self.published.unwrap_or(0),
-                publishedText: self.publishedText.unwrap_or(String::new()),
-                liveNow: self.liveNow.unwrap_or(false),
+                published_text: self.published_text.unwrap_or(String::new()),
+                live: self.live_now.unwrap_or(false),
                 paid: self.paid.unwrap_or(false),
                 premium: self.premium.unwrap_or(false),
                 author: self.author,
-                authorId: self.authorId,
-                authorUrl: self.authorUrl,
+                author_id: self.author_id,
+                author_url: self.author_url,
             },
             "playlist" => SearchItem::Playlist {
                 title: self.title.unwrap_or(String::new()),
-                playlistId: self.playlistId.unwrap_or(String::new()),
+                id: self.playlist_id.unwrap_or(String::new()),
                 author: self.author,
-                authorId: self.authorId,
-                authorUrl: self.authorUrl,
-                videoCount: self.videoCount.unwrap_or(0),
+                author_id: self.author_id,
+                author_url: self.author_url,
+                video_count: self.video_count.unwrap_or(0),
                 videos: self.videos.unwrap_or(Vec::new()),
             },
             "channel" => SearchItem::Channel {
-                author: self.author,
-                authorId: self.authorId,
-                authorUrl: self.authorUrl,
+                name: self.author,
+                id: self.author_id,
+                url: self.author_url,
 
-                authorThumbnails: self.authorThumbnails.unwrap_or(Vec::new()),
-                subCount: self.subCount.unwrap_or(0),
-                videoCount: self.videoCount.unwrap_or(0),
+                thumbnails: self.author_thumbnails.unwrap_or(Vec::new()),
+                subscribers: self.sub_count.unwrap_or(0),
+                video_count: self.video_count.unwrap_or(0),
                 description: self.description.unwrap_or(String::new()),
-                descriptionHtml: self.descriptionHtml.unwrap_or(String::new()),
+                description_html: self.description_html.unwrap_or(String::new()),
             },
             _ => SearchItem::Unknown(self),
         }
@@ -79,42 +90,42 @@ impl SearchItemTransition {
 pub enum SearchItem {
     Video {
         title: String,
-        videoId: String,
+        id: String,
         author: String,
-        authorId: String,
-        authorUrl: String,
-        lengthSeconds: u64,
-        videoThumbnails: Vec<VideoThumbnail>,
+        author_id: String,
+        author_url: String,
+        length: u64,
+        thumbnails: Vec<VideoThumbnail>,
         description: String,
-        descriptionHtml: String,
-        viewCount: u64,
+        description_html: String,
+        views: u64,
         published: u64,
-        publishedText: String,
-        liveNow: bool,
+        published_text: String,
+        live: bool,
         paid: bool,
         premium: bool,
     },
 
     Playlist {
         title: String,
-        playlistId: String,
+        id: String,
         author: String,
-        authorId: String,
-        authorUrl: String,
-        videoCount: u32,
+        author_id: String,
+        author_url: String,
+        video_count: u32,
         videos: Vec<SearchPlaylistVideo>,
     },
 
     Channel {
-        author: String,
-        authorId: String,
-        authorUrl: String,
+        name: String,
+        id: String,
+        url: String,
 
-        authorThumbnails: Vec<AuthorThumbnail>,
-        subCount: u32,
-        videoCount: u32,
+        thumbnails: Vec<AuthorThumbnail>,
+        subscribers: u32,
+        video_count: u32,
         description: String,
-        descriptionHtml: String,
+        description_html: String,
     },
 
     Unknown(SearchItemTransition),
@@ -123,7 +134,10 @@ pub enum SearchItem {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SearchPlaylistVideo {
     pub title: String,
-    pub videoId: String,
-    pub lengthSeconds: u32,
-    pub videoThumbnails: Vec<VideoThumbnail>,
+    #[serde(rename(serialize = "videoId", deserialize = "videoId"))]
+    pub id: String,
+    #[serde(rename(serialize = "lengthSeconds", deserialize = "lengthSeconds"))]
+    pub length: u32,
+    #[serde(rename(serialize = "videoThumbnails", deserialize = "videoThumbnails"))]
+    pub thumbnails: Vec<VideoThumbnail>,
 }
